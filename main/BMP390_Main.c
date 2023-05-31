e7812b9acb4f39add50b2b36ff5898701ff06452
Your Name <you@example.com>
2023-05-31 15:20:33 +0200

    Version gibt immer 0.5 Pa aus

--- main/BMP390_Main.c
+++ main/BMP390_Main.c
@@ -4,7 +4,7 @@
 #include <driver/i2c.h>
 
 #define I2C_MASTER_NUM    I2C_NUM_0
-#define I2C_MASTER_FREQ_HZ 100000
+#define I2C_MASTER_FREQ_HZ 400000
 #define I2C_MASTER_TX_BUF_DISABLE 0
 #define I2C_MASTER_RX_BUF_DISABLE 0
 #define I2C_MASTER_INTR_FLAG_NONE 0
@@ -13,7 +13,9 @@
 #define I2C_SDA_PIN     21
 #define BMP390_I2C_ADDR 0x77
 
-#define BMP390_REG_DATA     0x04
+#define BMP390_REG_PRESS_MSB 0x06
+#define BMP390_REG_PRESS_LSB 0x05
+#define BMP390_REG_PRESS_XLSB 0x04
 
 static const i2c_config_t i2c_config = {
     .mode = I2C_MODE_MASTER,
@@ -34,28 +36,22 @@ static void i2c_master_init()
 
 static void bmp390_read_data()
 {
-    uint8_t data[6];
-    int16_t temperature;
+    uint8_t data[3];
     uint32_t pressure;
 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (BMP390_I2C_ADDR << 1) | I2C_MASTER_WRITE, true);
-    i2c_master_write_byte(cmd, BMP390_REG_DATA, true);
+    i2c_master_write_byte(cmd, BMP390_REG_PRESS_XLSB, true);
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (BMP390_I2C_ADDR << 1) | I2C_MASTER_READ, true);
-    i2c_master_read(cmd, data, 6, I2C_MASTER_LAST_NACK);
+    i2c_master_read(cmd, data, 3, I2C_MASTER_LAST_NACK);
     i2c_master_stop(cmd);
     i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
     i2c_cmd_link_delete(cmd);
 
-    temperature = (int16_t)((data[3] << 8) | data[2]);
     pressure = ((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) | data[2];
 
-    // Verarbeite die Temperatur- und Druckwerte nach Bedarf
-    // ...
-
-    printf("Temperatur: %.2f °C\n", (float)temperature / 100.0);
     printf("Druck: %.2f Pa\n", (float)pressure / 256.0);
 }
 
