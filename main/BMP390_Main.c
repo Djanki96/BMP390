3d414157e4d7b90c72cfdefd102c14f150051ecd
Djanki96 <janik.rohrer@posteo.de>
2023-06-06 16:04:03 +0200

    Raw temperature and pressure

--- main/BMP390_Main.c
+++ main/BMP390_Main.c
@@ -13,9 +13,8 @@
 #define I2C_SDA_PIN     21
 #define BMP390_I2C_ADDR 0x77
 
-#define BMP390_REG_PRESS_MSB 0x06
-#define BMP390_REG_PRESS_LSB 0x05
 #define BMP390_REG_PRESS_XLSB 0x04
+#define BMP390_REG_TEMP_XLSB 0x07
 
 static const i2c_config_t i2c_config = {
     .mode = I2C_MODE_MASTER,
@@ -36,34 +35,32 @@ static void i2c_master_init()
 
 static void bmp390_read_data()
 {
-    uint8_t data[3];
+    uint8_t data[6];
     uint32_t pressure;
+    uint32_t temperature;
 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
 
-    //uint8_t data_send[2];
-
-    //data_send[0] = 0x04;
-    //data_send[1] = 0x05;
-
-    //i2c_master_write(cmd, &data_send, 2, false);
     i2c_master_write_byte(cmd, (BMP390_I2C_ADDR << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd, 0x1B, true);
     i2c_master_write_byte(cmd, 0b00110011, true);
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (BMP390_I2C_ADDR << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd, BMP390_REG_PRESS_XLSB, true);
+    i2c_master_write_byte(cmd, BMP390_REG_TEMP_XLSB, true);
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (BMP390_I2C_ADDR << 1) | I2C_MASTER_READ, true);
-    i2c_master_read(cmd, data, 3, I2C_MASTER_LAST_NACK);
+    i2c_master_read(cmd, data, 6, I2C_MASTER_LAST_NACK);
     i2c_master_stop(cmd);
     i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
     i2c_cmd_link_delete(cmd);
 
     pressure = ((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) | data[2];
+    printf("Druck:  %.0f hPa\n", (float)pressure);
 
-    printf("Druck: %.5f hPa\n", (float)pressure);
+    temperature = ((uint32_t)data[3] << 16) | ((uint32_t)data[4] << 8) | data[5];
+    printf("Temp:   %.0f °C\n", (float)temperature);
 }
 
 void app_main()
@@ -72,6 +69,6 @@ void app_main()
 
     while (1) {
         bmp390_read_data();
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
